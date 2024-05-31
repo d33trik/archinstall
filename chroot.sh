f5818307ec730a847ebefaa91de4c333d0447ee4
Claudio Dietrich <claudio@dietrich.dev.br>
2024-05-31 20:14:37 -0300

    Add bootloader setup

--- chroot.sh
+++ chroot.sh
@@ -84,6 +84,7 @@ main() {
 	setup_graphical_interface
 	setup_audio_interface
 	setup_network_interface
+	setup_bootloader
 }
 
 install_gum() {
@@ -182,6 +183,21 @@ setup_network_interface() {
 			systemctl enable ufw.service
 			systemctl start ufw.service
 			ufw enable
+		"
+}
+
+setup_bootloader() {
+	gum spin \
+		--title="Setting up the bootloader..." \
+		-- bash -c "
+			if [ \"$boot_mode\" = 1 ]; then
+				pacman -S --noconfirm grub efibootmgr
+				grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot
+			else
+				pacman -S --noconfirm grub
+				grub-install \"$block_device\"
+			fi
+			grub-mkconfig -o /boot/grub/grub.cfg
 		"
 }
 
