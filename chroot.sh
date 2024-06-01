5b5c0ce0c48704b61fc52bfaa17ea1315a7cd990
Claudio Dietrich <claudio@dietrich.dev.br>
2024-06-01 10:22:26 -0300

    Update user account setup
    Modify the user account setup function to use sed
    for uncommenting the sudoers line.

--- chroot.sh
+++ chroot.sh
@@ -111,7 +111,7 @@ setup_user_account() {
 			sleep 1
 			useradd -m -g wheel -s /bin/bash -c \"$user_full_name\" \"$user_username\"
 			echo \"$user_username:$user_password\" | chpasswd
-			echo \"%wheel ALL=(ALL) ALL\" >> /etc/sudoers
+			sed -i '/^# %wheel ALL=(ALL:ALL) ALL/s/^# //' /etc/sudoers
 		"
 }
 
