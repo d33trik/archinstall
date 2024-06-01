40e97078c3702bafc8f13251c8ecba41610bf519
Claudio Dietrich <claudio@dietrich.dev.br>
2024-06-01 02:07:17 -0300

    Fix indentation

--- packages.sh
+++ packages.sh
@@ -11,25 +11,25 @@ export GUM_SPIN_SPINNER_FOREGROUND=10
 export GUM_SPIN_TITLE_FOREGROUND=15
 
 main() {
-    local packages_to_install
-    local user_username
+	local packages_to_install
+	local user_username
 
-    while [[ $# -gt 0 ]]; do
-        case "$1" in
-            --packages-to-install)
-                packages_to_install="$2"
-                shift 2
-                ;;
-            --user-username)
-                user_username="$2"
-                shift 2
-                ;;
-            *)
-                echo "Unknown option: $1"
-                exit 1
-                ;;
-        esac
-    done
+	while [[ $# -gt 0 ]]; do
+		case "$1" in
+			--packages-to-install)
+				packages_to_install="$2"
+				shift 2
+				;;
+			--user-username)
+				user_username="$2"
+				shift 2
+				;;
+			*)
+				echo "Unknown option: $1"
+				exit 1
+				;;
+		esac
+	done
 }
 
 main "$@"
