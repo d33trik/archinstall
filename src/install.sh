38a656f55ec1379902c267eb99823a7034692bce
Claudio Dietrich <claudio@dietrich.dev.br>
2024-10-12 11:08:12 -0300

    Install git on the live system

--- src/install.sh
+++ src/install.sh
@@ -7,6 +7,7 @@ set -euo pipefail
 
 main() {
 	synchronize_package_databases
+	install_git
 }
 
 synchronize_package_databases() {
@@ -14,6 +15,14 @@ synchronize_package_databases() {
 	echo "Synchronizing package databases..."
 	sleep 1
 	pacman -Sy &> /dev/null
+	clear
+}
+
+install_git() {
+	clear
+	echo "Installing git..."
+	sleep 1
+	pacman -S --noconfirm --needed git &> /dev/null
 	clear
 }
 
