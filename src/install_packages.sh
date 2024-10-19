bd8eca28529c4f893675ebe34cf0e10a924e0e28
Claudio Dietrich <claudio@dietrich.dev.br>
2024-10-19 14:31:28 -0300

    Update package list
    - Add firefox

--- src/install_packages.sh
+++ src/install_packages.sh
@@ -92,6 +92,14 @@ install_chromium() {
 	"
 }
 
+install_firefox() {
+	gum spin \
+	--title="[$index/$total] Installing $package_name..." \
+	-- sudo -u "$user_username" bash -c "
+		sudo pacman -S --noconfirm --needed firefox
+	"
+}
+
 disable_sudo_execution_without_password() {
 	gum spin \
 		--title="Disabling sudo execution without a password..." \
