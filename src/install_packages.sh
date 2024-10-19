a3723a5fe0d5c06451f40b701cd2de8677dc2a11
Claudio Dietrich <claudio@dietrich.dev.br>
2024-10-19 00:47:06 -0300

    Enable sudo execution without password

--- src/install_packages.sh
+++ src/install_packages.sh
@@ -9,6 +9,18 @@ main() {
 	local install_dotfiles=${1:?}
 	local packages=${2:?}
 	local user_username=${3:?}
+
+	enable_sudo_execution_without_password
+}
+
+enable_sudo_execution_without_password() {
+	gum spin \
+		--title="Enabling sudo execution without a password..." \
+		-- bash -c "
+			sleep 1
+			sed -i '/^%wheel ALL=(ALL:ALL) ALL/s/^/# /' /etc/sudoers
+			sed -i '/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/s/^# //' /etc/sudoers
+		"
 }
 
 main "$@"
