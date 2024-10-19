151edbce2bb65c637fc7c95d713349acfb60e88c
Claudio Dietrich <claudio@dietrich.dev.br>
2024-10-19 16:31:31 -0300

    Update package list
    - Add remmina

--- src/install_packages.sh
+++ src/install_packages.sh
@@ -147,6 +147,14 @@ install_obsidian() {
 	"
 }
 
+install_remmina() {
+	gum spin \
+	--title="[$index/$total] Installing $package_name..." \
+	-- sudo -u "$user_username" bash -c "
+		sudo pacman -S --noconfirm --needed remmina freerdp
+	"
+}
+
 disable_sudo_execution_without_password() {
 	gum spin \
 		--title="Disabling sudo execution without a password..." \
