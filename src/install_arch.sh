b92b86be81e973320e36e3f11e760669201509b6
Claudio Dietrich <claudio@dietrich.dev.br>
2024-10-28 01:55:09 -0300

    Implement wipe method selection

--- src/install_arch.sh
+++ src/install_arch.sh
@@ -18,6 +18,7 @@ main() {
 	local hostname
 	local block_device
 	local swap_size
+	local wipe_method
 
 	display_welcome_message
 	select_keyboard_layout
@@ -35,6 +36,7 @@ main() {
 	get_hostname
 	select_block_device
 	get_swap_size
+	select_wipe_method
 }
 
 display_welcome_message() {
@@ -207,6 +209,20 @@ get_swap_size() {
 	)
 
 	[[ $swap_size =~ ^[0-9]+$ ]] || swap_size=$default_swap_size
+}
+
+select_wipe_method() {
+	local wipe_methods=(
+		"1 DD /dev/zero (Faster & Prevent Easy Recovery)"
+		"2 DD /dev/random (Slower & Prevent Hard Recovery)"
+		"3 No Need (The Device is Empty)"
+	)
+
+	wipe_method=$(
+		printf "%s\n" "${wipe_methods[@]}" |
+		gum choose \
+			--header="Select your preferred wipe method..."
+	)
 }
 
 main "$@"
