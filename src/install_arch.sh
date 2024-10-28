653d7c6c01c6ac4764d55d6f50d101e94cdd0a18
Claudio Dietrich <claudio@dietrich.dev.br>
2024-10-28 01:34:13 -0300

    Display welcome message

--- src/install_arch.sh
+++ src/install_arch.sh
@@ -6,7 +6,36 @@
 set -euo pipefail
 
 main() {
+	display_welcome_message
+}
 
+display_welcome_message() {
+	local prompt=$(
+		gum format \
+			--type="markdown" -- \
+			"$(gum style --bold --foreground="11" "Attention!")" \
+			"" \
+			"Welcome to my Arch Linux installation script!" \
+			"" \
+			"This script will guide you through an installation of Arch Linux" \
+			"based on my preferred settings." \
+			"" \
+			"However, feel free to modify it to fit your own needs." \
+			"" \
+			"$(gum style --bold --foreground="9" "Important Note:") Running this script will completely erase all data" \
+			"on the disk you choose for installation." \
+			"" \
+			"Are you ready to proceed?" |
+		gum style \
+			--border="normal" \
+			--margin="1" \
+			--padding="1 2" \
+			--border-foreground="7"
+	)
+
+	gum confirm \
+		--default="false" \
+		"$prompt"
 }
 
 main "$@"
