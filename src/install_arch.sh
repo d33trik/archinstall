63e3f409963ae4a0094cf99db1b9d8ed1bf7a344
Claudio Dietrich <claudio@dietrich.dev.br>
2024-10-28 02:28:12 -0300

    Mount filesystems

--- src/install_arch.sh
+++ src/install_arch.sh
@@ -47,6 +47,7 @@ main() {
 	wipe_block_device
 	partition_block_device
 	format_partitions
+	mount_filesystems
 }
 
 display_welcome_message() {
@@ -370,6 +371,17 @@ format_partitions() {
 			[[ \"$boot_mode\" == 1 ]] && mkfs.fat -F32 \"${block_device}1\"
 			mkswap \"${block_device}2\"
 			mkfs.ext4 \"${block_device}3\"
+		"
+}
+
+mount_filesystems() {
+	gum spin \
+		--title="Mounting filesystems..." \
+		-- bash -c "
+			sleep 1
+			mount \"${block_device}3\" /mnt
+			swapon \"${block_device}2\"
+			[[ \"$boot_mode\" == 1 ]] && mkdir -p /mnt/boot && mount \"${block_device}\"1 /mnt/boot
 		"
 }
 
