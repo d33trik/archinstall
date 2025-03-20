9adc766898068ac1ba33b1761be2f621179d698b
Claudio Dietrich <d33trik@gmail.com>
2025-03-20 14:51:08 -0300

    Install packages

--- packages.sh
+++ packages.sh
@@ -8,6 +8,13 @@ set -euo pipefail
 main() {
 	synchronize_package_databases
 	install_yay
+	install_packages
+	set_up_fish
+	set_up_bluetooth
+	set_up_pulse_audio
+	set_up_firewall
+	set_up_podman
+	set_up_virt_manager
 }
 
 synchronize_package_databases() {
@@ -25,6 +32,65 @@ install_yay() {
 		sudo pacman -Rs --noconfirm go
 		cd "$working_directory"
 	fi
+}
+
+install_packages() {
+	local packages
+	if [ -f "data/packages.yaml" ]; then
+			packages="data/packages.yaml"
+	else
+			packages="archinstall/data/packages.yaml"
+	fi
+
+	awk '/^ *- / {print $2}' "$packages" | while IFS= read -r package; do
+		# Skip empty lines and comments
+		[[ -z "$package" || "$package" == \#* ]] && continue
+
+		# Install package
+		yay -S --noconfirm --needed "$package"
+	done
+}
+
+set_up_fish() {
+	chsh -s /usr/bin/fish
+}
+
+set_up_bluetooth() {
+	sudo systemctl enable bluetooth.service
+	sudo systemctl start bluetooth.service
+}
+
+set_up_pulse_audio() {
+	sudo gpasswd -a $(whoami) audio
+	sudo rm -f /etc/pulse/default.pa.d/noise-cancellation.pa
+	cat <<EOF >> sudo /etc/pulse/default.pa.d/noise-cancellation.pa
+### Enable Echo/Noise-Cancellation
+load-module module-echo-cancel use_master_format=1 aec_method=webrtc aec_args=\"analog_gain_control=0 digital_gain_control=1\" source_name=echoCancel_source sink_name=echoCancel_sink
+set-default-source echoCancel_source
+set-default-sink echoCancel_sink
+EOF
+	pulseaudio -k
+	pulseaudio --start
+}
+
+set_up_firewall() {
+	sudo systemctl enable ufw.service
+	sudo systemctl start ufw.service
+	sudo ufw enable
+}
+
+set_up_podman() {
+	sudo systemctl enable podman-restart.service
+	sudo rm -f /etc/containers/registries.conf.d/00-shortnames.conf
+	sudo rm -f /etc/containers/registries.conf.d/00-unqualified-search-registries.conf
+	echo 'unqualified-search-registries = ["docker.io"]' | sudo tee -a /etc/containers/registries.conf.d/00-unqualified-search-registries.conf
+}
+
+set_up_virt_manager() {
+	sudo gpasswd -a $(whoami) libvirt
+	sudo systemctl enable libvirtd.socket
+	sudo rm -f /etc/libvirt/network.conf
+	echo 'firewall_backend="iptables"' | sudo tee -a /etc/libvirt/network.conf
 }
 
 main "$@"
