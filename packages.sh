3a6abbd95bc2d029df81d82ec451776a18903056
Claudio Dietrich <d33trik@gmail.com>
2025-03-21 18:09:07 -0300

    Fix pulse audio set up

--- packages.sh
+++ packages.sh
@@ -63,14 +63,10 @@ set_up_bluetooth() {
 set_up_pulse_audio() {
 	sudo gpasswd -a $(whoami) audio
 	sudo rm -f /etc/pulse/default.pa.d/noise-cancellation.pa
-	cat <<EOF >> sudo /etc/pulse/default.pa.d/noise-cancellation.pa
-### Enable Echo/Noise-Cancellation
-load-module module-echo-cancel use_master_format=1 aec_method=webrtc aec_args=\"analog_gain_control=0 digital_gain_control=1\" source_name=echoCancel_source sink_name=echoCancel_sink
+	echo '### Enable Echo/Noise-Cancellation
+load-module module-echo-cancel use_master_format=1 aec_method=webrtc aec_args="analog_gain_control=0 digital_gain_control=1" source_name=echoCancel_source sink_name=echoCancel_sink
 set-default-source echoCancel_source
-set-default-sink echoCancel_sink
-EOF
-	pulseaudio -k
-	pulseaudio --start
+set-default-sink echoCancel_sink' | sudo tee -a /etc/pulse/default.pa.d/noise-cancellation.pa
 }
 
 set_up_podman() {
